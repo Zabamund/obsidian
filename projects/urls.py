177956fd34e2e12dc505491c8ddb41c35fc565cc
Zabamund <fracgeol@gmail.com>
2019-10-07 17:12:01 +0200

    Add paths to projects urls

---
+++ projects/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.allprojects, name='allprojects'),
+    path('<int:project_id>/', views.detail, name='detail'),
+]
