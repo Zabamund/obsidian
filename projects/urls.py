670fc0db8c6183401336243b0f01ddacace39150
Zabamund <fracgeol@gmail.com>
2019-10-08 09:24:33 +0200

    Remove allprojects path from projects>urls.py

--- projects/urls.py
+++ projects/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.allprojects, name='allprojects'),
     path('<int:project_id>/', views.detail, name='detail'),
 ]
